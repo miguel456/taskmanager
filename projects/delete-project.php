e90e4f72cc7f636136e0f75a96c97c4dfd718d79
Miguel Nogueira <me@nogueira.codes>
2025-05-28 20:49:47 +0100

    feat(projects): add project deletion logic

---
+++ projects/delete-project.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Projects\Project;
+
+require_once realpath(__DIR__ . '/../app/bootstrap.php');
+
+$project_id = $_POST['project_id'];
+$project = new Project();
+
+
+if (empty($project_id) && !$project->project_exists($project_id)) {
+    flash_message('Dados inesperados', 'O projeto a apagar não existe ou não é válido.', 'error');
+    response('/projects', 'Bad Request', [], 400);
+    die;
+}
+
+if ($project->delete_project($project_id)) {
+    flash_message('Sucesso', 'Projeto eliminado com sucesso.');
+    response('/projects');
+    die;
+}
+
+flash_message('Erro Interno', 'Por motivos desconhecidos não foi possível apagar o projeto. Tente novamente mais tarde.', 'error');
+response('/projects');
